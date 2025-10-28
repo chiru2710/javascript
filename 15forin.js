0a83ae8430bd320580b062c3cc64000ff35a1003
chiru2710 <jangilichiranjeevi2@gmail.com>
2025-10-28 14:08:59 +0530

    changes

--- 15forin.js
+++ 15forin.js
@@ -9,3 +9,18 @@ for(key in cat){
     total+=cat[key]
 }
 console.log(total)
+//for of
+// for of only iterate the arrays only if we need to
+//iterate the objects we need to use the object methods
+let str="chiru";
+for(value of str){
+    console.log(value)
+}
+let ages=[20,30,40,50];
+let newArray=[]
+for(age of ages){
+    if(age>30){
+        newArray.push(age)
+    }
+}
+console.log(newArray)
