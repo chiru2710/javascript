a916093351f08aa93fa15ef1a2177b8dcf637dfa
chiru2710 <jangilichiranjeevi2@gmail.com>
2025-11-01 10:48:37 +0530

    form events

--- 16event.js
+++ 16event.js
@@ -1,21 +1,33 @@
-//Mouse events
-let mouse=document.getElementById('mouse')
-let h1=document.getElementById('h1')
-mouse.addEventListener('mousedown',()=>
-{
-    mouse.innerHTML='Mouse down'
-   h1.textContent='Mouse Down'
-   h1.style.color="green" 
-})
+// //Mouse events
+// let mouse=document.getElementById('mouse')
+// let h1=document.getElementById('h1')
+// mouse.addEventListener('mousedown',()=>
+// {
+//     mouse.innerHTML='Mouse down'
+//    h1.textContent='Mouse Down'
+//    h1.style.color="green" 
+// })
 
-mouse.addEventListener('mouseup',()=>
+// mouse.addEventListener('mouseup',()=>
+// {
+//     mouse.innerHTML='Mouse up'
+//     h1.textContent='Mouse up'
+//    h1.style.color="red"
+// })
+// mouse.addEventListener('mouseover',()=>
+// {
+//     mouse.style.color="black"
+//     mouse.style.backgroundColor="yellow"
+// })
+
+// form Event
+let form=document.getElementById('form');
+let sub=document.getElementById('subject');
+let text=document.getElementById('text');
+let box=document.getElementById('con');
+
+form.addEventListener('submit',(e)=>
 {
-    mouse.innerHTML='Mouse up'
-    h1.textContent='Mouse up'
-   h1.style.color="red"
-})
-mouse.addEventListener('mouseover',()=>
-{
-    mouse.style.color="black"
-    mouse.style.backgroundColor="yellow"
+    e.preventDefault()
+    console.log(text.value)
 })
